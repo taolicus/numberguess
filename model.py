000e88f3d15b4f9085c54f099f8270b1c0349795
Tao <taozamorano@gmail.com>
2025-06-06 12:18:57 +0100

    format

--- model.py
+++ model.py
@@ -103,10 +103,10 @@ def test(model):
   """)
 
   def imshow(img):
-      img = img / 2 + 0.5 # unnormalize
-      npimg = img.numpy()
-      plt.imshow(np.transpose(npimg, (1, 2, 0)))
-      plt.show()
+    img = img / 2 + 0.5 # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
 
   # Get some random test images
   dataiter = iter(testloader)
