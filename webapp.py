89de7e7414700cec0935827eefc6285c0212c83a
Tao <taozamorano@gmail.com>
2025-06-06 10:56:28 +0100

    update layout and add true label required

--- webapp.py
+++ webapp.py
@@ -17,9 +17,16 @@ if "confidence" not in st.session_state:
   st.session_state.confidence = "--"
 if "label" not in st.session_state:
   st.session_state.label = None
+if "logs" not in st.session_state:
+  st.session_state.logs = db.fetch_logs()
+if "feedback" not in st.session_state:
+  st.session_state.feedback = ""
 
 def guess():
-  # print('Guessing...')
+  if st.session_state.label == None:
+    st.session_state.feedback = "Please enter a label"
+    return
+
   if canvas_state.image_data is not None:
     img = Image.fromarray(canvas_state.image_data.astype("uint8"), mode="RGBA")
     img = img.resize((28, 28))
@@ -40,7 +47,9 @@ def guess():
     output = model(img_tensor)
     probabilities = F.softmax(output, dim=1)
     st.session_state.prediction = torch.argmax(probabilities, dim=1).item()
-    st.session_state.confidence = math.trunc(probabilities[0][st.session_state.prediction].item() * 100)
+    st.session_state.confidence = math.trunc(
+      probabilities[0][st.session_state.prediction].item() * 100
+    )
     # print(f'Prediction: {st.session_state.prediction}')
     # save prediction to postgresql
     db.log({
@@ -48,42 +57,58 @@ def guess():
       'prediction': st.session_state.prediction,
       'label': st.session_state.label
     })
+    st.session_state.logs = db.fetch_logs()
+
+  st.session_state.label = None
+  st.session_state.feedback = ""
 
 st.title('Digit Recogniser')
-
 st.write('Draw a digit:')
-canvas_state = st_canvas(
-  stroke_width=10,
-  background_color='#000000',
-  fill_color='#FFFFFF',
-  stroke_color='#FFFFFF',
-  width=300,
-  height=300,
-  drawing_mode='freedraw',
-  key='canvas'
-)
 
-st.write('## Prediction:', st.session_state.prediction)
+col1, col2 = st.columns(2)
 
-st.write(f'Confidence: {st.session_state.confidence}%')
+with col1:
+  canvas_state = st_canvas(
+    stroke_width=10,
+    background_color='#000000',
+    fill_color='#FFFFFF',
+    stroke_color='#FFFFFF',
+    width=300,
+    height=300,
+    drawing_mode='freedraw',
+    key='canvas'
+  )
 
-label = st.number_input('True label:', value=None, min_value=0, max_value=9, key="label")
+with col2:
+  st.write('## Prediction:', st.session_state.prediction)
 
-button = st.button("Guess", on_click=guess)
+  st.write(f'Confidence: {st.session_state.confidence}%')
+
+  label = st.number_input(
+    'True label:',
+    value=None,
+    min_value=0,
+    max_value=9,
+    key="label"
+  )
+
+  button = st.button("Guess", on_click=guess)
+
+  st.write(st.session_state.feedback)
 
 # db.create_logs_table()
 
-logs = db.fetch_logs()
+
 # print(logs)
 
 logs_table = """
 ---
 ## History
-| timestamp | pred | label |
+| Timestamp | Pred | Label |
 |-----------|------|-------|
 """
 
-for row in logs:
+for row in st.session_state.logs:
     _, timestamp, prediction, label = row
     timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
     logs_table += f"| {timestamp} | {prediction} | {label} |\n"
